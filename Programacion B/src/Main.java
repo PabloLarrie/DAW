3d8fc12fd4b8b3ac441d5e0450a83863f81ff8c6
PabloLarrie <pablo_14_pont@hotmail.com>
2021-09-28 12:11:19 +0200

    correct PB

--- Programacion B/src/Main.java
+++ Programacion B/src/Main.java
@@ -18,7 +18,8 @@ public class Main {
             System.out.println(" 3. Resta ");
             System.out.println(" 4. Multiplicación ");
             System.out.println(" 5. División ");
-            System.out.println(" 5. Números antes desde 0 ");
+            System.out.println(" 6. Números hasta " + num1);
+            System.out.println(" 7. Números desde " + num2+ " hasta 0");
             System.out.println(" 0. Salir ");
             System.out.println("   - Elige una opción:");
             opcion = sc.nextInt();
@@ -55,6 +56,15 @@ public class Main {
                     System.out.println("Los números son: ");
                     for (int i = 0; i <= num1; i++) {
                         System.out.print(i + "; ");
+                    }
+                    System.out.println();
+                    break;
+                }
+                case 7: {
+                    System.out.println("Contar números atrás desde" + num2+"a 0");
+                    System.out.println("Los números son: ");
+                    while (num2>0) {
+                        System.out.print(num2-- +"; ");
                     }
                     System.out.println();
                     break;
